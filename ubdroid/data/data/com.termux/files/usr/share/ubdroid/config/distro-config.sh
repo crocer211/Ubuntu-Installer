dc40d59a5ab7ef4537d64293a08245c252cc391e
crocer211 <68352570+crocer211@users.noreply.github.com>
2020-11-13 15:08:36 -0500

    Add files via upload

---
+++ ubdroid/data/data/com.termux/files/usr/share/ubdroid/config/distro-config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+### Prepare the system for post-installation
+### Things to do:
+### 1. Update and Upgrade the system
+### 2. Install Necessary Packages
+### 3. Configure User Accounts
+### 4. Add the user to sudoers file
+### 5. Clean up.
+set -e -u
+
+## Install Packages
+apt update
+apt upgrade -y || true
+apt install nano sudo busybox udisks2 dbus-x11 locales pulseaudio -y || true
+apt autoremove -y || true
+apt clean
+
+## Fix Problems
+echo ""
+echo "Exporting libgcc_s.so.1 as a walkaround for Tigervnc"
+echo "export LD_PRELOAD=$(find /usr/lib -name libgcc_s.so.1)" > /etc/profile.d/walkaround.sh
+echo "Adding /sbin path for non-root users"
+echo "export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/games:/bin:/sbin:/usr/bin:/usr/sbin:/usr/games" > /etc/profile.d/sbin.sh
+echo "export LANG=C.UTF-8" > /etc/profile.d/langenv.sh
+echo "export PULSE_SERVER=127.0.0.1" > /etc/profile.d/pulse.sh
+
+## Configure udisks2 as if udisks2 interrupts apt
+echo ""
+echo "Configuring Udisks2.... "
+echo "" > /var/lib/dpkg/info/udisks2.postinst
+dpkg --configure -a
+
+## Setup User Accounts
+echo "Configuring User Accounts.... "
+read -p "Enter your UNIX username (lower case): " username
+useradd -s /bin/bash -m $username
+passwd $username
+echo "Adding the user to sudoers.d file"
+echo "$username ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/$username
+echo "$username" > /etc/userinfo.rc
